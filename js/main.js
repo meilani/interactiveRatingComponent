d43159256df354c3b705db874a848e5e46b18e46
meilani <lauraho2006@gmail.com>
2022-11-06 16:00:48 -0500

    css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
 const thanksHeading = 'Thank you!'
+let rating = 0
+let ratingText = `You selected ${rating} out of 5`
+let thanksText = 'We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!'
