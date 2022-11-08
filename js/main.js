f20d5508b375d1bb44ef6541e5ce8f6d1fc1ad7d
meilani <lauraho2006@gmail.com>
2022-11-07 21:51:00 -0500

    button functionality and style

--- js/main.js
+++ js/main.js
@@ -1,4 +1,35 @@
-const thanksHeading = 'Thank you!'
-let rating = 0
-let ratingText = `You selected ${rating} out of 5`
-let thanksText = 'We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!'
+let ratingNumber = 0
+
+const ratings = document.querySelectorAll('.circle')
+const submit = document.querySelector('#submit')
+const content = document.querySelector('#content')
+
+for (let rating of ratings) {
+    rating.addEventListener('click', ()=> {
+        ratingNumber = rating.dataset.rate
+        ratings.forEach(element => {
+            element.classList.remove('active-rating')
+        })
+        rating.classList.add('active-rating')
+    })
+}
+
+submit.addEventListener('click', ()=> {
+    content.style.opacity = 0
+    content.classList.add('result')
+    content.innerHTML = `
+        <img 
+            src="../images/illustration-thank-you.svg" 
+            width="144" 
+            height="96"
+            alt="an illustration of a phone with a credit card going in and a receipt coming out"
+            loading="lazy" 
+        >
+        <div class="selected">You selected ${ratingNumber} out of 5</div>
+        <h1>Thank you!</h1>
+        <p>
+            We appreciate you taking the time to give a rating. If you ever need more support, don’t hesitate to get in touch!
+        </p>
+    `
+    content.style.opacity = 1;
+})
